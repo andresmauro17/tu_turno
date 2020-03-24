fe342294a96f73915a7373fb2bf5ef8aa59cab90
andresmauro17 <andresmauro17@gmail.com>
2020-03-24 11:17:38 -0500

    changing seeds

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,11 +19,36 @@ class UsersTableSeeder extends Seeder
 
             User::create([
                 'name'           => 'Admin',
-                'email'          => 'admin@admin.com',
-                'password'       => bcrypt('password'),
+                'email'          => 'admin@mail.com',
+                'password'       => bcrypt('admin'),
                 'remember_token' => Str::random(60),
                 'role_id'        => $role->id,
             ]);
+
+            User::create([
+                'name'           => 'andresmauro17',
+                'email'          => 'andresmauro17@hotmail.com',
+                'password'       => bcrypt('thebest1'),
+                'remember_token' => Str::random(60),
+                'role_id'        => $role->id,
+            ]);
+
+            User::create([
+                'name'           => 'aletop',
+                'email'          => 'aletop@hotmail.com',
+                'password'       => bcrypt('admin'),
+                'remember_token' => Str::random(60),
+                'role_id'        => $role->id,
+            ]);
+
+            User::create([
+                'name'           => 'luzmarina',
+                'email'          => 'luzmarina@mail.com',
+                'password'       => bcrypt('admin'),
+                'remember_token' => Str::random(60),
+                'role_id'        => $role->id,
+            ]);
+
         }
     }
 }
