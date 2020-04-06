ffb6e0438abdacb13dbfe412dd4a3f6a30c88165
andresmauro17 <andresmauro17@gmail.com>
2020-04-06 11:52:14 -0500

    error when not modules

--- app/Http/Controllers/AtentionController.php
+++ app/Http/Controllers/AtentionController.php
@@ -11,11 +11,19 @@ use Illuminate\Support\Facades\Auth;
 class AtentionController extends Controller
 {
     public function index(Request $request){
-       
-        $services = Service::all();
-        $module = Auth::User()->module()->with('diligences')->get()[0];
 
-        return view('turns.atention', compact('services','module'));
+        if(count(Auth::User()->module()) == 0){
+            dd('chupapijas');
+            $services = Service::all();
+            $module = Auth::User()->module()->with('diligences')->get()[0];
+    
+            return view('turns.atention', compact('services','module'));
+        }else{
+            // dd('no hay nada');
+            abort(403, 'No exiten modulos asociados a este usuario!');
+        }
+       
+        
     }
     
 }
